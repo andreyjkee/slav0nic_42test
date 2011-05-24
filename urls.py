a20422f10132502c540cf0e53ece5889720adc83
slav0nic <slav0nic0@gmail.com>
2011-05-24 13:30:47 +0300

    ticket:5 added login form

--- urls.py
+++ urls.py
@@ -6,5 +6,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'', include('basicapp.urls', namespace='basicapp')),
 )
