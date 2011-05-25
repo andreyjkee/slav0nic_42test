1cb5ff92c8c97a4fc7a5af21f7b08ea931de5aaa
slav0nic <slav0nic0@gmail.com>
2011-05-25 13:08:45 +0300

    ticket:8 admin_change_url tag implementation; link added to main page

--- basicapp/templatetags/basicapp_extras.py
+++ basicapp/templatetags/basicapp_extras.py
@@ -1,5 +1,7 @@
 #coding: utf-8
 from django import template
+from django.core.urlresolvers import reverse
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -9,5 +11,5 @@ def admin_change_url(obj):
       ticket:8 Tag return `admin change url` for obj
     '''
 
-
-    return ''
+    url = reverse('admin:%s_%s_change' % (obj._meta.app_label, obj._meta.module_name), args=[obj.id])
+    return mark_safe(u'<a href="%s">(%s)</a>' % (url, obj.__unicode__()))
