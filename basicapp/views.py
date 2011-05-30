6b20e6e571f2f410a3cef83f6303d57ba8ad994a
slav0nic <slav0nic0@gmail.com>
2011-05-30 12:30:55 +0300

    ticket:13 fix automerge; pep8

--- basicapp/views.py
+++ basicapp/views.py
@@ -5,7 +5,8 @@ from django.contrib.auth.decorators import login_required
 from django.http import Http404, HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.utils import simplejson as json
-from basicapp.models import RequestLog, UserProfile, PRIORITY_CHOICES
+from django.shortcuts import redirect
+from basicapp.models import RequestLog, UserProfile
 from basicapp.forms import EditProfileForm as EditProfileFormOrigin
 
 
