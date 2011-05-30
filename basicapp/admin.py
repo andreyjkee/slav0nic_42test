e21c25a90d37dac00b19e4b1cb20967312d21d6e
slav0nic <slav0nic0@gmail.com>
2011-05-30 11:47:00 +0300

    ticket:3 pep8

--- basicapp/admin.py
+++ basicapp/admin.py
@@ -7,9 +7,10 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'skype', 'jabber', 'birthday']
     raw_id_fields = ['user'] # whithout this your can get fun on big DB ;)
 
+
 class RequestLogAdmin(admin.ModelAdmin):
     list_display = ['date', 'method', 'path', 'remote_addr']
 
+
 admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(RequestLog, RequestLogAdmin)
-
